bea338443d925c1fe48ad847e509ca3af58e7cad
yanamadalaraju <150237963+yanamadalaraju@users.noreply.github.com>
2025-01-02 12:01:42 +0530

    added lead

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Lead from "./components/pages/Lead";
 
 function App() {
   return (
     <div className="App">
-      
+      <Lead />
     </div>
   );
 }
