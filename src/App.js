e83b36374412cb2922f2c9e1c9484841660c77fc
unknown <sharvanikokkonda@gmail.com>
2025-01-02 11:59:50 +0530

    added employee and manager

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Employee from './components/pages/Employee';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Manager from './components/pages/Manager';
+
 
 function App() {
   return (
     <div className="App">
-      
+     <Employee/>
+     <Manager/>
     </div>
   );
 }
